6904dfe755675e34e30d9010690fb4fe7fc2a41a
hixam <hisami27@gmail.com>
2016-03-26 01:51:37 +0100

    user resource added

---
+++ service/src/main/java/edu/upc/eetac/dsa/beeter/BeeterMediaType.java
@@ -0,0 +1,12 @@
+package edu.upc.eetac.dsa.beeter;
+
+/**
+ * Created by hixam on 25/03/16.
+ */
+public interface BeeterMediaType {
+    public final static String BEETER_AUTH_TOKEN = "application/vnd.dsa.beeter.auth-token+json";
+    public final static String BEETER_USER = "application/vnd.dsa.beeter.user+json";
+    public final static String BEETER_STING = "application/vnd.dsa.beeter.sting+json";
+    public final static String BEETER_STING_COLLECTION = "application/vnd.dsa.beeter.sting.collection+json";
+    public final static String BEETER_ROOT = "application/vnd.dsa.beeter.root+json";
+}
